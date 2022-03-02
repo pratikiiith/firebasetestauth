3cb83e6e337f6d044176dd7dcf0a747f2c6daa88
pratik <pratiktiwari94@gmail.com>
2022-03-02 16:56:05 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRARV3htv75MDhnI--N874iXyzPLEpo_8",
-  authDomain: "friendlychat-53530.firebaseapp.com",
-  databaseURL: "https://friendlychat-53530-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "friendlychat-53530",
-  storageBucket: "friendlychat-53530.appspot.com",
-  messagingSenderId: "384669647506",
-  appId: "1:384669647506:web:5a695e170dffacc56bdc4b"
+  ....
 };
 
 // Initialize Firebase
